70c95fcc1236331b234b772e9e37566fe28ae4d4
ane-noelle <anenoelle60@gmail.com>
2025-09-01 11:35:18 -0300

    dia3

--- GMusic/App.js
+++ GMusic/App.js
@@ -19,7 +19,42 @@ export default function App() {
       </View>
 
       <View>
-        <Slider/>
+        <Text style={[styles.songContent, styles.songTittle]}>
+          Titulo da Música
+        </Text>
+        <Text style={[styles.songContent, styles.songArtist]}>
+          Autor da Música
+        </Text>
+      </View>
+
+      <View>
+        <Slider
+          style={styles.progressBar}
+          value={10}
+          minimumValue={0}
+          maximumValue={100}
+          thumbTintColor='#FFD369'
+          minimumTrackTintColor='#FFD369'
+          maximumTrackTintColor='#fff'
+          onSlidingComplete={() => {}}
+        />
+        <View style={styles.progressLevelDuration}>
+          <Text style={styles.progressLabelText}>00:00</Text>
+          <Text style={styles.progressLabelText}>00:00</Text>
+        </View>
+      </View>
+
+      <View style={styles.musicControlsContainer}>
+        <TouchableOpacity>
+          <Ionicons name='play-skip-back-outline' size={35} color="#FFD369" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionicons name='pause-circle' size={75} color="#FFD369" />
+        </TouchableOpacity> 
+        <TouchableOpacity>
+          <Ionicons name='play-skip-foward-outline' size={35} color="#FFD369" />
+        </TouchableOpacity>
+
       </View>
 
       </View>
@@ -91,4 +126,38 @@ const styles = StyleSheet.create({
     height: '100%',
     borderRadius: 15
   },
+  songContent: {
+    textAlign: 'center',
+    color: '#EEEEE',
+  },
+  songTittle: {
+  fontSize: 18,
+  fontWeight:'600',
+  },
+  songArtist: {
+    fontSize: 16,
+    fontWeight: '300',
+  },
+  progressBar: {
+    width: 350,
+    height: 40,
+    marginTop: 20,
+  },
+  progressLevelDuration: {
+    width: 340,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  progressLabelText: {
+    color: '#fff',
+    fontWeight: '500',
+  },
+  musicControlsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '60%',
+    marginTop: 10,
+  }
+
 });
